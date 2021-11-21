cc83e3286bc904fb89d15e2f6721cf3b0154a63e
Zian Asti Dwiyanti <ziandwiasti23@gmail.com>
2021-11-21 20:35:58 +0700

    Upload

---
+++ P3_2_1204049/ChildForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace P3_2_1204049
+{
+    partial class ChildForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.ExitButtom = new System.Windows.Forms.Button();
+            this.DisplayButton = new System.Windows.Forms.Button();
+            this.OutputGroupBox = new System.Windows.Forms.GroupBox();
+            this.DateOption = new System.Windows.Forms.RadioButton();
+            this.TimeOption = new System.Windows.Forms.RadioButton();
+            this.OutputGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.OutputLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.OutputLabel.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputLabel.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.OutputLabel.Location = new System.Drawing.Point(0, 0);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(278, 32);
+            this.OutputLabel.TabIndex = 0;
+            this.OutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ExitButtom
+            // 
+            this.ExitButtom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExitButtom.Location = new System.Drawing.Point(153, 59);
+            this.ExitButtom.Name = "ExitButtom";
+            this.ExitButtom.Size = new System.Drawing.Size(75, 23);
+            this.ExitButtom.TabIndex = 2;
+            this.ExitButtom.Text = "E&xit";
+            this.ExitButtom.UseVisualStyleBackColor = true;
+            this.ExitButtom.Click += new System.EventHandler(this.ExitButtom_Click);
+            // 
+            // DisplayButton
+            // 
+            this.DisplayButton.Location = new System.Drawing.Point(51, 59);
+            this.DisplayButton.Name = "DisplayButton";
+            this.DisplayButton.Size = new System.Drawing.Size(75, 23);
+            this.DisplayButton.TabIndex = 1;
+            this.DisplayButton.Text = "&Display";
+            this.DisplayButton.UseVisualStyleBackColor = true;
+            this.DisplayButton.Click += new System.EventHandler(this.DisplayButton_Click);
+            // 
+            // OutputGroupBox
+            // 
+            this.OutputGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.OutputGroupBox.Controls.Add(this.DateOption);
+            this.OutputGroupBox.Controls.Add(this.TimeOption);
+            this.OutputGroupBox.Location = new System.Drawing.Point(39, 152);
+            this.OutputGroupBox.Name = "OutputGroupBox";
+            this.OutputGroupBox.Size = new System.Drawing.Size(200, 104);
+            this.OutputGroupBox.TabIndex = 3;
+            this.OutputGroupBox.TabStop = false;
+            this.OutputGroupBox.Text = "Choose Output";
+            // 
+            // DateOption
+            // 
+            this.DateOption.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.DateOption.Location = new System.Drawing.Point(16, 23);
+            this.DateOption.Name = "DateOption";
+            this.DateOption.Size = new System.Drawing.Size(168, 24);
+            this.DateOption.TabIndex = 0;
+            this.DateOption.TabStop = true;
+            this.DateOption.Text = "Display Current D&ate";
+            this.DateOption.UseVisualStyleBackColor = true;
+            this.DateOption.CheckedChanged += new System.EventHandler(this.DateOption_CheckedChanged);
+            // 
+            // TimeOption
+            // 
+            this.TimeOption.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.TimeOption.Location = new System.Drawing.Point(16, 57);
+            this.TimeOption.Name = "TimeOption";
+            this.TimeOption.Size = new System.Drawing.Size(168, 24);
+            this.TimeOption.TabIndex = 1;
+            this.TimeOption.TabStop = true;
+            this.TimeOption.Text = "Display Current &Time";
+            this.TimeOption.UseVisualStyleBackColor = true;
+            this.TimeOption.CheckedChanged += new System.EventHandler(this.TimeOption_CheckedChanged);
+            // 
+            // ChildForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Controls.Add(this.OutputGroupBox);
+            this.Controls.Add(this.DisplayButton);
+            this.Controls.Add(this.ExitButtom);
+            this.Controls.Add(this.OutputLabel);
+            this.Name = "ChildForm";
+            this.Text = "Child Form";
+            this.OutputGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.Button ExitButtom;
+        private System.Windows.Forms.Button DisplayButton;
+        private System.Windows.Forms.GroupBox OutputGroupBox;
+        private System.Windows.Forms.RadioButton DateOption;
+        private System.Windows.Forms.RadioButton TimeOption;
+    }
+}
